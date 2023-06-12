44a30b931b4f21cb1336c71a70d27915d2bfd1f9
mahmoud <mahmoudhassanamin123@gmail.com>
2023-06-12 14:21:06 +0300

    add data validation to user model

--- models/user/index.js
+++ models/user/index.js
@@ -3,15 +3,25 @@ const mongoose = require("mongoose");
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
+        maxLength:15,
+        required:true,
+        match:/^[a-zA-z0-9]+$/,
+        unique:true
     },
     password:{
         type:String,
+        match:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
     },
     email:{
         type:String,
+        match:/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+        unique:true,
     },
     mobile:{
         type:String,
+        minLength:11,
+        match:/^(012|011|010)/,
+        unique:true
     }
 },{
     timestamps:true
